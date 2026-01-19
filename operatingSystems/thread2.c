afd35997127f81ee2100278a806cb99bbf3f96c0
Harish Anandh <harish.anandh@gmail.com>
2026-01-19 11:16:17 +0530

    Add files via upload

---
+++ operatingSystems/thread2.c
@@ -0,0 +1,23 @@
+#define _POSIX_C_SOURCE 199309L
+#include<stdio.h>
+#include<time.h>
+#include<pthread.h>
+#define ITER 2000000000LL
+void *work(void* arg){
+   volatile long long i;
+   for(int i = 0;i< ITER;i++);
+   return NULL;
+   }
+int main(){
+  struct timespec start,end;
+  clock_gettime(CLOCK_MONOTONIC,&start);
+  pthread_t t1,t2,t3,t4;
+  pthread_create(&t1,NULL,work,NULL);
+  pthread_create(&t2,NULL,work,NULL);
+  pthread_create(&t3,NULL,work,NULL);
+  pthread_create(&t4,NULL,work,NULL);
+  clock_gettime(CLOCK_MONOTONIC,&end);
+  double time = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) /1e9;
+  printf("Time taken %.2f seconds\n",time);
+  return 0;
+  }
